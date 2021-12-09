3a66dcff190ec66de31818f6daccc74b5b09cfc5
rfy <1020972669@qq.com>
2021-12-09 11:38:34 +0800

    yi

---
+++ gmall-vvv/src/main/java/com.atguigu.gmall.vvv/GmallVvvApplication.java
@@ -0,0 +1,10 @@
+package com.atguigu.gmall.vvv;
+
+import org.springframework.boot.SpringApplication;
+
+
+public class GmallVvvApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GmallVvvApplication.class,args);
+    }
+}
